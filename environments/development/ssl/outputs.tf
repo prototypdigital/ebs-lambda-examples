13580c2b87208e2975e41859e2ebfaeef43befbd
Vlatko Vlahek <vlatko@prototyp.digital>
2024-06-27 12:50:09 +0200

    Removed ssl

--- environments/development/ssl/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "frontend" {
-  description = "SSL certificate for the frontend application"
-  value       = module.ssl_app
-}
-
-output "api" {
-  description = "SSL certificate for the API application"
-  value       = module.ssl_api
-}
